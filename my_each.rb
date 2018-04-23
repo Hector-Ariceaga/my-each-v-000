27d3968d76b9af437b403fbb399b2bd96f1b4562
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-23 17:54:08 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  block_given?
+  i = 0
+  
+  while i < array.length
+  yield
+  i += 1
+  end
+else
+  array
+end
 end
